b4ac195b47c17b80cda881435e2613f5d73508b4
chaso13 <chaso13@yahoo.com>
2024-12-09 15:58:30 -0500

    day2 complete

--- Day_1/main.py
+++ Day_1/main.py
@@ -1,54 +1,56 @@
-#Advent of Code Day 1
+"""Day 1 of Advent of Code 2024.
+The goal of part 1 is to find the sum of the absolute differences between the
+two lists of integers after the lists have been sorted from least to greatest.
+
+The goal of part 2 is to find the sum of each integer in the first list
+multiplied by the count of its appearance in the second list. By using
+dictionaries as counters, we can quickly multiply each unique integer in the
+left list by it's occurance in the left list and the right list to not make
+repeated calls to the right lists dictionary count.
+"""
 
 
+class AdventDay1:
 
-import math 
+    def __init__(self):
+        """Read in the input file and sort the two lists and create counters.
+        The counters will be used to multiply the integer by the count of its
+        appearance in each list."""
+        self.left = []
+        self.right = []
+        with open('Day_1/inputFile.txt', 'r') as f:
+            for line in f.readlines():
+                l, r = line.split()
+                self.left.append(int(l))
+                self.right.append(int(r))
+        self.left.sort()
+        self.right.sort()
+        self.left_counter = self._val_counter(self.left)
+        self.right_counter = self._val_counter(self.right)
+        self.part1 = 0
+        self.part2 = 0
 
-#parse input
+    def get_diff_sum(self):
+        """Get the sum of the absolute differences between the two lists."""
+        for i in range(len(self.left)):
+            self.part1 += abs(self.left[i] - self.right[i])
 
-def read_input(input_file_dir):
-    with open(input_file_dir, 'r') as file:
-        lines = file.readlines()
-    list1 = []
-    list2 = []
-    for line in lines:
-        data = line.split()
-        list1.append(int(data[0]))
-        list2.append(int(data[1]))
-    return list1, list2
+    def get_similarity_score(self):
+        """Get the sum of each integer in the left list multiplied by the count
+        of its appearance in the right list."""
+        for key, val in self.left_counter.items():
+            self.part2 += key * val * self.right_counter.get(key, 0)
 
-#Part 1
-def calculate_total_distance(list1, list2):
-    list1.sort()
-    list2.sort()
-    total_distance = 0
-    for i in range(len(list1)):
-        total_distance += (math.fabs(list1[i] - list2[i]))
-    return total_distance
+    def _val_counter(self, list):
+        """Create a dictionary of the count of each integer in the list."""
+        counts = {}
+        for x in list:
+            counts[x] = counts.get(x, 0) + 1
+        return counts
 
-#part 2
-def calculate_similartiy_score(list1, list2):
-    number_occurrence_map = {}
-    similarity_score = 0
-    for number in list2:
-        if number not in number_occurrence_map:
-            number_occurrence_map[number] = 1
-        else:
-            current_count = number_occurrence_map[number]
-            number_occurrence_map[number] = current_count + 1
-    for number in list1:
-        if number in number_occurrence_map:
-            count = number * number_occurrence_map[number]
-            similarity_score += count
-    return similarity_score
-        
-#main function 
 
 if __name__ == '__main__':
-    list1, list2 = read_input('./inputFile.txt')
-
-    totalDistance = calculate_total_distance(list1, list2)
-    score = calculate_similartiy_score(list1, list2)
-
-    print(totalDistance)
-    print(score)
+    day1 = AdventDay1()
+    day1.get_diff_sum()
+    day1.get_similarity_score()
+    print(day1.part1, day1.part2)
